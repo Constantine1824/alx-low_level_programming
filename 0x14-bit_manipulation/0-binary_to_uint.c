cacd45c3a720e7d8cf226282a6d8708ca95feb78
Ayomide <ayomidet905@gmail.com>
2023-11-24 06:52:28 -0800

    first commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Convert a binary string to unsigned int
+ * @b: The binary string to be converted
+ *
+ * Return: unsigned int if the number is binary and not NULL. 0 otherwise
+ */
+unsigned int binary_to_uint(const char *b) 
+{
+    unsigned int result = 0;
+
+    while (*b != '\0') {
+        if ((*b != '0' && *b != '1') || *b == '\0') {
+            return 0;
+        };
+        result = (result<<1) + (*b - '0');
+        b++;
+    };
+    return result;
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
